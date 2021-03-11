f2994b5c5449b97b2cb5b8a7436f1bcb915053ac
Raissa Havens <raissahavens@gmail.com>
2021-03-10 21:20:51 -0600

    changes

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,27 +1,31 @@
 package org.launchcode.spaday.controllers;
 
+// import jdk.javadoc.internal.tool.Messager;
 import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
+    public String processAddUserForm(Model model, @Valid @ModelAttribute User user, Errors errors, String verify) {
         if (user.getPassword().equals(verify)) {
+            model.addAttribute("error", "Passwords do not match");
            return "user/index";
+        } else if (errors.hasErrors()){
+            return "user/index";
         }
         else {
             model.addAttribute("error", "Passwords do not match");
--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,20 +1,37 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class User {
+
+    @NotEmpty(message ="Please enter a user name.")
+    @Size(min = 5, max = 15)
     private String username;
+
+    @Email(message = "Please enter a valid email")
     private String email;
+
+    @NotEmpty
+    @Min(min = 6)
     private String password;
+
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
+
 
     public String getUsername() {
         return username;
@@ -38,5 +55,13 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
     }
 }
